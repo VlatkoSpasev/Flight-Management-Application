5582f09d7a8d08fdec3b57605b11b7ec2f61be0a
Marko <markopetrov1212@gmail.com>
2025-05-04 22:21:09 +0200

    add views and templates

--- FlightApp/views.py
+++ FlightApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Flight
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    # Show flights in the past
+    flights = Flight.objects.filter(date__lte=datetime.now().date())
+    context = {'flight_list': flights, 'app_name': 'FlightApp'}
+    return render(request, 'index.html', context)
+
+def details(request, flight_id):
+    flight = Flight.objects.filter(id=flight_id).first()
+    context = {'flight_data': flight, 'app_name': 'FlightApp'}
+    return render(request, 'details.html', context)
